2f4540ee11cafa1e55604c8765e11bb85953bee0
Arne Maier <Arne_Maier@gmx.de>
2019-02-18 17:26:37 +0100

    Add editing

--- src/features/CakeList/CakeList.tsx
+++ src/features/CakeList/CakeList.tsx
@@ -1,16 +1,18 @@
 import * as React from 'react';
 import { Heading, Button, Container } from 'rebass';
 import { connect } from 'react-firebase';
+import styled from 'styled-components';
 
 import { App } from '../../services/firebase';
 import addCurrentUser, { InjectedCurrentUserProps } from '../../hocs/addCurrentUser';
 import Input from '../../common/Input/Input';
 import Label from '../../common/Label';
+import Icon from '../../common/Icon';
 import { Cake, cakeServerResponseToCake, CakeServerResponse } from './cake.types';
 
 
 interface State {
-	title: string;
+	cake: string;
 	bakedBy: string;
 }
 
@@ -28,12 +30,12 @@ class CakeList extends React.Component<Props, State> {
 	constructor(props: Props) {
 		super(props);
 		this.state = {
-			title: '',
+			cake: '',
 			bakedBy: '',
 		};
 	}
 
-	updateTitle = (title: React.ChangeEvent<HTMLInputElement>) => this.setState({ title: title.target.value });
+	updateCake = (cake: React.ChangeEvent<HTMLInputElement>) => this.setState({ cake: cake.target.value });
 	updateBakedBy = (bakedBy: React.ChangeEvent<HTMLInputElement>) => this.setState({ bakedBy: bakedBy.target.value });
 
 	removeCake = (key: string) => {
@@ -42,12 +44,12 @@ class CakeList extends React.Component<Props, State> {
 
 	addCake = () => {
 		this.props.addCake({
-			title: this.state.title,
+			title: this.state.cake,
 			bakedBy: this.state.bakedBy,
 			creator: this.props.currentUser && this.props.currentUser.uid,
 		});
 		this.setState({
-			title: '',
+			cake: '',
 			bakedBy: '',
 		});
 	}
@@ -88,7 +90,7 @@ class CakeList extends React.Component<Props, State> {
 						<tr>
 							<td>
 								<form onSubmit={this.handleSubmit}>
-									<Input id="title" required placeholder="Was?" type="text" value={this.state.title} onChange={this.updateTitle} />
+									<Input id="title" required placeholder="Was?" type="text" value={this.state.cake} onChange={this.updateCake} />
 								</form>
 							</td>
 							<td>
@@ -97,17 +99,93 @@ class CakeList extends React.Component<Props, State> {
 								</form>
 							</td>
 							<td>
-								<Button onClick={this.addCake} disabled={this.state.title === '' || this.state.bakedBy === ''}>
+								<Button onClick={this.addCake} disabled={this.state.cake === '' || this.state.bakedBy === ''}>
 									Add
 								</Button>
 							</td>
 						</tr>
 					</tbody>
 				</table>
+				{this.renderPrototypeOfTable()}
 			</Container>
 		);
 	}
+
+	renderPrototypeOfTable = () => (
+		<div>
+			<Table>
+				<thead>
+					<tr>
+						<TableHead>Kuchen</TableHead>
+						<TableHead>Name</TableHead>
+						<TableHead/>
+					</tr>
+				</thead>
+				<tbody>
+
+					<tr>
+						<TableData>Pfirsich</TableData>
+						<TableData>Arne</TableData>
+						<TableData><Icon alt="remove cake" name="trash" /></TableData>
+					</tr>
+					<tr>
+						<TableData>Pfirsich</TableData>
+						<TableData>Arne</TableData>
+						<TableData><Icon alt="remove cake" name="trash" /></TableData>
+					</tr>
+					<tr>
+						<TableData>Pfirsich</TableData>
+						<TableData>Arne</TableData>
+						<TableData><Icon alt="remove cake" name="trash" /></TableData>
+					</tr>
+					<tr>
+						<TableDataInput>Name:</TableDataInput>
+						<TableDataInput>
+								<Input placeholder="Max" type="text" id="name" value={this.state.bakedBy} onChange={this.updateBakedBy} />
+						</TableDataInput>
+						<TableDataInput/>
+					</tr>
+					<tr>
+						<TableDataInput>Kuchen:</TableDataInput>
+						<TableDataInput>
+								<Input placeholder="Apfelkuchen" type="text" id="name" value={this.state.cake} onChange={this.updateCake} />
+						</TableDataInput>
+						<TableDataInput><Icon alt="add cake" name="plus" /></TableDataInput>
+					</tr>
+				</tbody>
+			</Table>
+		</div>
+	)
 }
+
+const Table = styled.table`
+	width: 100%;
+	text-align: left;
+	box-shadow: 0px 0px 15px 0px rgba(0,0,0,0.5);
+	border-collapse: collapse;
+
+	tr:nth-child(odd) {
+		background-color: white;
+	}
+	tr:nth-child(even) {
+		background-color: grey;
+	}
+`;
+
+const TableHead = styled.th`
+	padding: 12px 25px;
+	background-color: #363636;
+	color: white;
+`;
+
+const TableData = styled.td`
+	padding: 12px 25px;
+`;
+
+const TableDataInput = TableData.extend`
+	background-color: white;
+`;
+
 
 const mapFirebaseToProps = (props: Props, ref: any, firebase: App) => ({
 	cakes: `cakes`,
